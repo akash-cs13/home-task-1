db5319bf17c065837706f2d031baae5f6ce41608
Akash CS <84382172+akash-cs13@users.noreply.github.com>
2023-10-12 23:08:52 +0530

    home task 1 commit

---
+++ src/com/akash/Main.java
@@ -0,0 +1,8 @@
+package com.akash;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Home Assignment 1 \nBy: Akash Cuntur Shrinivasmurthy\nDevelopment Environment: JDK 21 SE");
+    }
+}
